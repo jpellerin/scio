77dabeed8fe5f7baf76450d32723f2825d91dfa0
jpellerin <none@none>
2011-06-03 09:13:47 -0400

    Bumped version, pinned datetutil

--- setup.py
+++ setup.py
@@ -14,11 +14,11 @@ Topic :: Software Development :: Object Brokering""".split("\n")
 
 setup(
     name='Scio',
-    version='0.9',
+    version='0.9.1',
     author_email='oss@leapfrogdevelopment.com',
     url='http://bitbucket.org/leapfrogdevelopment/scio/overview',
     description='Scio is a humane SOAP client',
-    install_requires=['lxml>=2.2', 'python-dateutil'],
+    install_requires=['lxml>=2.2', 'python-dateutil>=1.4,<1.5'],
     tests_require=['nose>=1.0', 'Sphinx>=1.0'],
     packages=find_packages(),
     classifiers=CLASSIFIERS,
