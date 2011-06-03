c91463382dc5a71e144f88ce64486bf92102161c
jpellerin <none@none>
2011-06-03 09:42:19 -0400

    Made dateutil version requirement depend on python version

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+import sys
 
 CLASSIFIERS = """\
 Development Status :: 5 - Production/Stable
@@ -12,13 +13,18 @@ Programming Language :: Python :: 2.6
 Programming Language :: Python :: 2.7
 Topic :: Software Development :: Object Brokering""".split("\n")
 
+if sys.version_info >= (2, 6):
+    dateutil_version = '>=1.4,<2.0'
+else:
+    dateutil_version = '>=1.4,<1.5'
+
 setup(
     name='Scio',
     version='0.9.1',
     author_email='oss@leapfrogdevelopment.com',
     url='http://bitbucket.org/leapfrogdevelopment/scio/overview',
     description='Scio is a humane SOAP client',
-    install_requires=['lxml>=2.2', 'python-dateutil>=1.4,<1.5'],
+    install_requires=['lxml>=2.2', 'python-dateutil%s' % dateutil_version],
     tests_require=['nose>=1.0', 'Sphinx>=1.0'],
     packages=find_packages(),
     classifiers=CLASSIFIERS,
